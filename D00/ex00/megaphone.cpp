35a609f3a6b86554af3d79638e21ed97cf140a91
Nicolas MATTERA <nmattera@e3r5p2.clusters.42paris.fr>
2022-11-07 16:24:33 +0100

    ex00 test

--- D00/ex00/megaphone.cpp
+++ D00/ex00/megaphone.cpp
@@ -6,14 +6,31 @@
 /*   By: nmattera <nmattera@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/04 16:12:04 by nmattera          #+#    #+#             */
-/*   Updated: 2022/11/07 14:45:06 by nmattera         ###   ########.fr       */
+/*   Updated: 2022/11/07 16:22:46 by nmattera         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <cstring>
+#include <cstdio>
 
+using namespace std;
 
-int	main()
+int	main(int ac, char **av)
 {
-	std::cout << "42\n";
+	int	i;
+	int	word;
+	
+	if (ac == 1)
+		cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	for (word = 1; word < ac; word++)
+	{
+		for (i = 0; i < (int)strlen(av[word]); i++)
+		{
+			
+			char a = toupper(av[word][i]);
+			cout << a;
+		}
+	}
+	cout << endl;
 }
