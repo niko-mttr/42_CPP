088b5b3cea50093bf6729f735fff681ea31eaf08
Nicolas MATTERA <nmattera@e3r4p4.clusters.42paris.fr>
2022-11-12 21:04:11 +0100

    d01 test

---
+++ D01/ex00/Zombie.hpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Zombie.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nmattera <nmattera@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/12 19:14:52 by nmattera          #+#    #+#             */
+/*   Updated: 2022/11/12 19:27:14 by nmattera         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ZOMBIE_HPP
+# define ZOMBIE_HPP
+
+# include <iostream>
+# include <string>
+
+class Zombie
+{
+	private:
+		std::string	name;
+	public:
+		Zombie();
+		~Zombie();
+		void announce( void );
+};
+
+#endif
