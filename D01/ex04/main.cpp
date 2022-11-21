ddb83546dc7b3ea5dd1ea16c180bceea7c8e9ecd
Nicolas MATTERA <nmattera@e3r4p4.clusters.42paris.fr>
2022-11-21 19:01:59 +0100

    d01-ex04

--- D01/ex04/main.cpp
+++ D01/ex04/main.cpp
@@ -6,11 +6,50 @@
 /*   By: nmattera <nmattera@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/20 17:25:28 by nmattera          #+#    #+#             */
-/*   Updated: 2022/11/21 18:29:10 by nmattera         ###   ########.fr       */
+/*   Updated: 2022/11/21 19:01:08 by nmattera         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "sed.hpp"
+
+std::string ft_action(std::string save, std::string s1, std::string s2, int start)
+{
+	
+	start -= s1.length();
+	for(int i = 0; s2[i]; i++)
+	{
+		save[start] = s2[i];
+		start++;
+	}
+	return (save);
+}
+
+std::string ft_replace(std::string save, std::string s1, std::string s2)
+{
+	std::string nouveau;
+	int i = 0;
+	int counter = 0;
+
+	while (save[i])
+	{
+		counter = 0;
+		while (save[i] == s1[counter])
+		{
+				break ;
+			i++;
+			counter++;
+		}
+		i -= counter; 
+		if (counter == (int)(s1.length() - 1))
+		{
+			ft_action(save, s1, s2, i);
+			i += s2.length();
+		}
+		nouveau[i] = save[i];
+		i++;
+	}
+	return (nouveau);
+}
 
 void	ft_cpy(char *file, char *out, std::string s1, std::string s2)
 {
@@ -26,14 +65,16 @@ void	ft_cpy(char *file, char *out, std::string s1, std::string s2)
         while(getline(in_file, save))
 		{
 			if (!save.find(s1, 0))
-            	out_file << s2 << std::endl;
+            	out_file << ft_replace(save, s1, s2) << std::endl;
 			else
             	out_file << save << std::endl;
 		}
 		in_file.close();
     }
     else
+	{
         std::cout << "Unable to open the file!" << std::endl;
+	}
 	out_file.close();
 }
 
