c198e56861f1d525cc6e87e49e770212edf675de
niko-mttr <93621217+niko-mttr@users.noreply.github.com>
2022-11-22 14:42:25 +0100

    d01-ex05

---
+++ D01/ex05/Harl.hpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jcalon <jcalon@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/10 19:30:21 by jcalon            #+#    #+#             */
+/*   Updated: 2022/08/10 19:42:26 by jcalon           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef HARL_HPP
+# define HARL_HPP
+
+# include <iostream>
+# include <string>
+
+class Harl
+{
+	private :
+		void	debug( void );
+		void	info( void );
+		void	warning( void );
+		void	error( void );
+
+	public :
+		Harl(void);
+		~Harl(void);
+
+		void	complain( std::string level );
+};
+
+#endif
---
+++ D01/ex05/main.cpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jcalon <jcalon@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/10 19:26:57 by jcalon            #+#    #+#             */
+/*   Updated: 2022/09/06 16:10:09 by jcalon           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Harl.hpp"
+
+int	main(void)
+{
+	Harl harl;
+
+	harl.complain("DEBUG");
+	harl.complain("INFO");
+	harl.complain("WARNING");
+	harl.complain("ERROR");
+	return (0);
+}
