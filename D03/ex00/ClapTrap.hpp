fe5acae525f5715fc77c80a869a3e93283b52da3
Nicolas MATTERA <nmattera@e2r1p3.clusters.42paris.fr>
2022-11-30 19:02:52 +0100

    d02-ok

---
+++ D03/ex00/ClapTrap.hpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nmattera <nmattera@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/30 14:34:48 by nmattera          #+#    #+#             */
+/*   Updated: 2022/11/30 14:39:30 by nmattera         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+class ClapTrap
+{
+    private:
+    public:
+        ClapTrap();
+        ClapTrap(const ClapTrap &src);
+        ClapTrap &operator=(const ClapTrap &src);
+        ~ClapTrap();
+
+        void attack(const std::string& target);
+        void takeDamage(unsigned int amount);
+        void beRepaired(unsigned int amount);
+};
