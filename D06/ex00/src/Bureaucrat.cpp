f821b34c4afc4e81906e03b8359f5aa96941865e
Nicolas MATTERA <nmattera@e2r1p14.clusters.42paris.fr>
2022-12-07 22:36:02 +0100

    d06

---
+++ D06/ex00/src/Bureaucrat.cpp
@@ -0,0 +1,122 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.cpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nmattera <nmattera@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/05 18:08:31 by nmattera          #+#    #+#             */
+/*   Updated: 2022/12/06 15:36:29 by nmattera         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Bureaucrat.hpp"
+
+Bureaucrat::Bureaucrat()
+{
+    std::cout << "New Bureaucrat..." << std::endl;
+    this->name = "Didou";
+    this->grade = 3;
+}
+
+Bureaucrat::Bureaucrat(std::string nameGive, int gradeGive)
+{
+    std::cout << "New Bureaucrat..." << std::endl;
+    this->grade = gradeGive;
+    try
+    {
+        if (this->grade < 1)
+            throw Bureaucrat::GradeTooHighException();
+    }
+    catch(const std::exception& e)
+    {
+        this->grade = 1;
+        std::cerr << e.what() << '\n';
+    }
+    try
+    {
+        if (this->grade > 150)
+        {
+            throw Bureaucrat::GradeTooLowException();
+        }
+    }
+    catch(const std::exception& e)
+    {
+        this->grade = 150;
+        std::cerr << e.what() << '\n';
+    }
+    if (!nameGive.length())
+    {
+        std::cout << "Wrong name... standard name given : Philou" << std::endl;
+        this->name = "Philou";
+    }
+    else
+        this->name = nameGive;
+}
+
+Bureaucrat::Bureaucrat(const Bureaucrat &src): name(src.getName())
+{
+    std::cout << "New Bureaucrat..." << std::endl;
+    *this = src;
+}
+
+Bureaucrat & Bureaucrat::operator=(const Bureaucrat &src)
+{
+    this->grade = src.grade;
+    std::cout << "Cpy Bureaucrat..." << std::endl;
+    return *this;
+}
+
+Bureaucrat::~Bureaucrat()
+{
+    std::cout << "Delete Bureaucrat..." << std::endl;
+}
+
+std::string Bureaucrat::getName()const
+{
+    return this->name;
+}
+
+int Bureaucrat::getGrade()const
+{
+    return this->grade;
+}
+
+void Bureaucrat::upGrade()
+{
+    this->grade--;
+    try
+    {
+        if (grade < 1)
+            throw Bureaucrat::GradeTooHighException();
+    }
+    catch(const std::exception& e)
+    {
+        this->grade++;
+        std::cerr << e.what() << '\n';
+    }
+}
+
+void Bureaucrat::lowGrade()
+{
+    this->grade++;
+    try
+    {
+        if (grade > 150)
+        {
+            throw Bureaucrat::GradeTooLowException();
+        }
+    }
+    catch(const std::exception& e)
+    {
+        this->grade--;
+        std::cerr << e.what() << '\n';
+    }
+    
+}
+
+std::ostream & operator<<(std::ostream &o, const Bureaucrat &src)
+{
+    o << src.getName() << ", Bureaucrat grade " << src.getGrade();
+    return o;
+}
---
+++ D06/ex00/src/main.cpp
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nmattera <nmattera@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/30 14:32:19 by nmattera          #+#    #+#             */
+/*   Updated: 2022/12/06 16:36:44 by nmattera         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Bureaucrat.hpp"
+
+void testInit()
+{
+    std::cout << std::endl  << "**************************" << std::endl;
+    std::cout<< "*** TEST INITIALISATION ***" << std::endl;
+    std::cout << std::endl << "UP GRADE" << std::endl;
+    Bureaucrat test("David", -15);
+    std::cout << test << std::endl;
+    std::cout << std::endl << "DOWN GRADE" << std::endl;
+    Bureaucrat test2("Sandrine", 166);
+    std::cout << test2 << std::endl;
+}
+
+void testSimple()
+{
+    std::cout << std::endl  << "**************************" << std::endl;
+    std::cout << "*** TEST MODIFICATION ***" << std::endl;
+    std::cout << std::endl << "UP GRADE" << std::endl;
+    Bureaucrat test("David", 1);
+    test.upGrade();
+    std::cout << test << std::endl;
+    std::cout << std::endl << "DOWN GRADE" << std::endl;
+    Bureaucrat test2("Sandrine", 150);
+    std::cout << test2 << std::endl;
+    test2.lowGrade();
+    std::cout << test2 << std::endl;
+}
+
+int main()
+{
+    testSimple();
+    testInit();
+}
