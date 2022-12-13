9b450475bd542b12301b2e1d8aa5625a4add1534
Nicolas MATTERA <nmattera@e2r5p7.clusters.42paris.fr>
2022-12-13 17:41:36 +0100

    d07-ex02

---
+++ D07/ex02/includes/Array.hpp
@@ -0,0 +1,73 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Array.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nmattera <nmattera@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/13 12:16:28 by nmattera          #+#    #+#             */
+/*   Updated: 2022/12/13 16:38:17 by nmattera         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ARRAY_HPP
+# define ARRAY_HPP
+
+#include <iostream>
+
+template <typename T>
+class Array
+{
+    private:
+        int _n;
+        T* _tab;
+        
+    public:
+        Array(): _n(0), _tab(0) {std::cout << "Default constructor called" << std::endl;}
+        Array(unsigned int n): _n(n), _tab(new T[n]()) { std::cout << "Constructor called" << std::endl;}
+        Array(const Array &src)
+        {
+            std::cout << "Copy constructor called" << std::endl;
+            *this = src;
+        }
+        ~Array()
+        {
+            std::cout << "Destructor called" << std::endl;
+            if (this->_n > 0)
+                delete[] this->_tab;
+        }
+        Array & operator=(const Array &src)
+        {
+            if (this == &src)
+                return *this;
+            if (this->_tab)
+                delete[] this->_tab;
+            this->_n = src._n;
+            this->_tab = new T[src._n]();
+            for (int i = 0; i < src._n; i++)
+                this->_tab[i] = src._tab[i];
+            return *this;
+        }
+        
+        int size(){return (this->_n);}
+        
+        T operator[](int n)
+        {
+            if ((n) >= this->_n || _n < 0)
+            {
+                throw WrongRangeException();
+            }
+            return (this->_tab[n]);
+        }
+        
+        class WrongRangeException : public std::exception
+        {
+            public:
+                virtual const char * what()const throw()
+                {
+                    return "Wrong range give to size()";
+                }
+        };
+};
+
+#endif
---
+++ D07/ex02/src/main.cpp
@@ -0,0 +1,69 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nmattera <nmattera@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/30 14:32:19 by nmattera          #+#    #+#             */
+/*   Updated: 2022/12/13 16:37:43 by nmattera         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Array.hpp"
+
+#define MAX_VAL 12
+
+int main()
+{
+    // Array<int> test(3);
+    // test[1] = 6;
+    // std::cout << test[2] << std::endl;
+
+    Array<int> numbers(MAX_VAL);
+    int* mirror = new int[MAX_VAL];
+    srand(time(NULL));
+    for (int i = 0; i < MAX_VAL; i++)
+    {
+        const int value = rand();
+        numbers[i] = value;
+        mirror[i] = value;
+    }
+    //SCOPE
+    {
+        Array<int> tmp = numbers;
+        Array<int> test(tmp);
+    }
+
+    for (int i = 0; i < MAX_VAL; i++)
+    {
+        if (mirror[i] != numbers[i])
+        {
+            std::cerr << "didn't save the same value!!" << std::endl;
+            return 1;
+        }
+    }
+    try
+    {
+        numbers[-2] = 0;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    try
+    {
+        numbers[MAX_VAL] = 0;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+
+    for (int i = 0; i < MAX_VAL; i++)
+    {
+        numbers[i] = rand();
+    }
+    delete [] mirror;//
+    return 0;
+}
